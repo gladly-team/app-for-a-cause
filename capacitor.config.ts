6469e41b2b8995196a1f0c3a85bc51026a214b3d
Spicer Matthews <spicer@gladly.localdomain>
2025-01-17 14:58:43 -0800

    Changed the app title

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "io.ionic.starter",
-  appName: "Gladly Mobile",
+  appName: "App for a Cause",
   webDir: "build",
   plugins: {
     CapacitorHttp: {
