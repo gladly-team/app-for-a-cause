0c8a6f80941017385be5b4ed24cc76c5f48a1f3c
Spicer Matthews <spicer@gladly.localdomain>
2025-01-27 14:31:22 -0800

    Messed around with splash screens and such

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,6 +8,22 @@ const config: CapacitorConfig = {
     CapacitorHttp: {
       enabled: true,
     },
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
   },
 };
 
