0c64b7035a11c1c78be70b5911852a0c331544f5
Spicer Matthews <me@spicermatthews.com>
2025-07-03 13:33:07 -0700

    fixed up issue with branch.io

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,25 @@
 package io.gladly.appforacause;
 
+import android.content.Intent;
+import android.os.Bundle;
+import co.boundstate.BranchDeepLinks;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
+import io.branch.referral.Branch;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Branch.getInstance();
+    registerPlugin(BranchDeepLinks.class);
+  }
+
+  @Override
+  protected void onNewIntent(Intent intent) {
+    this.setIntent(intent);
+    super.onNewIntent(intent);
+  }
+}
